79c1a85a4bc9e1b29ca4d6f8aed84e96450845e3
Kaeycie <52530662+Kaeycie@users.noreply.github.com>
2025-09-23 14:45:13 +0200

    Added Schedule

--- src/app/Details.js
+++ src/app/Details.js
@@ -4,69 +4,52 @@ import SwipeGallery from './SwipeGallery';
 
 const scheduleData = [
 	{
-		time: '4:00pm',
+		time: '14:00',
 		title: 'Ceremony',
-		location: 'Bulawayo City Hall Gardens',
-		address: '8th Avenue, Bulawayo, Zimbabwe',
+		location: 'Sparrow Boutique Hotel',
+		address: 'Bulawayo, Zimbabwe',
 		directionsLink:
-			'http://maps.google.com/maps?q=Bulawayo+City+Hall+Gardens,+8th+Avenue,+Bulawayo,+Zimbabwe',
+			'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3831.510268539291!2d28.6247321!3d-20.2270526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1eb557bd003ff96f%3A0x836ddfc55f0ce060!2sSparrow%20Boutique%20Hotel!5e1!3m2!1sen!2szw!4v1757867183557!5m2!1sen!2szw',
 	},
 	{
-		time: '4:45pm',
-		title: 'Group & Family Photos',
-		location: 'Bulawayo City Hall Gardens',
-		address: '8th Avenue, Bulawayo, Zimbabwe',
+		time: '15:30',
+		title: 'Photos',
+		location: 'Sparrow Boutique Hotel',
+		address: 'Bulawayo, Zimbabwe',
 		directionsLink:
-			'http://maps.google.com/maps?q=Bulawayo+City+Hall+Gardens,+8th+Avenue,+Bulawayo,+Zimbabwe',
-		description: 'Join us for formal family and group photos after the ceremony.',
+			'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3831.510268539291!2d28.6247321!3d-20.2270526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1eb557bd003ff96f%3A0x836ddfc55f0ce060!2sSparrow%20Boutique%20Hotel!5e1!3m2!1sen!2szw!4v1757867183557!5m2!1sen!2szw',
 	},
 	{
-		time: '5:30pm',
-		title: 'Welcome Drinks & Canapés',
-		location: 'The Kingdom Hotel Lounge',
-		address: '26 Selous Avenue, Bulawayo, Zimbabwe',
+		time: '17:30',
+		title: 'Reception',
+		location: 'Sparrow Boutique Hotel',
+		address: 'Bulawayo, Zimbabwe',
 		directionsLink:
-			'http://maps.google.com/maps?q=The+Kingdom+Hotel,+26+Selous+Avenue,+Bulawayo,+Zimbabwe',
-		description: 'Please let us know if you will be around so we can cater for everyone.',
+			'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3831.510268539291!2d28.6247321!3d-20.2270526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1eb557bd003ff96f%3A0x836ddfc55f0ce060!2sSparrow%20Boutique%20Hotel!5e1!3m2!1sen!2szw!4v1757867183557!5m2!1sen!2szw',
 	},
 	{
-		time: '6:30pm',
-		title: 'Reception Begins',
-		location: 'The Kingdom Hotel Ballroom',
-		address: '26 Selous Avenue, Bulawayo, Zimbabwe',
-		directionsLink:
-			'http://maps.google.com/maps?q=The+Kingdom+Hotel,+26+Selous+Avenue,+Bulawayo,+Zimbabwe',
-	},
-	{
-		time: '6:45pm',
-		title: 'Cocktail Hour / Mingling',
-		description: 'Guests enjoy drinks and light snacks before dinner.',
-	},
-	{
-		time: '7:30pm',
-		title: 'Dinner Service',
-		description:
-			'Three-course meal with pre-selected options: starch, protein, salad, dessert, and drinks.',
-	},
-	{
-		time: '8:30pm',
-		title: 'Speeches & Toasts',
-		description: 'Family and close friends give speeches, followed by a champagne toast.',
-	},
-	{
-		time: '9:00pm',
-		title: 'First Dance & General Dancing',
-		description: 'The newlyweds have their first dance, followed by open dancing for all guests.',
-	},
-	{
-		time: '10:30pm',
+		time: '19:00',
 		title: 'Cake Cutting',
-		description: 'Join the couple for the ceremonial cake cutting.',
+		location: 'Sparrow Boutique Hotel',
+		address: 'Bulawayo, Zimbabwe',
+		directionsLink:
+			'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3831.510268539291!2d28.6247321!3d-20.2270526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1eb557bd003ff96f%3A0x836ddfc55f0ce060!2sSparrow%20Boutique%20Hotel!5e1!3m2!1sen!2szw!4v1757867183557!5m2!1sen!2szw',
 	},
 	{
-		time: '11:30pm',
-		title: 'Send-Off',
-		description: 'Farewell to guests, with sparklers or confetti.',
+		time: '20:00',
+		title: 'Dance',
+		location: 'Sparrow Boutique Hotel',
+		address: 'Bulawayo, Zimbabwe',
+		directionsLink:
+			'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3831.510268539291!2d28.6247321!3d-20.2270526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1eb557bd003ff96f%3A0x836ddfc55f0ce060!2sSparrow%20Boutique%20Hotel!5e1!3m2!1sen!2szw!4v1757867183557!5m2!1sen!2szw',
+	},
+	{
+		time: '23:00',
+		title: 'Grand Exit',
+		location: 'Sparrow Boutique Hotel',
+		address: 'Bulawayo, Zimbabwe',
+		directionsLink:
+			'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3831.510268539291!2d28.6247321!3d-20.2270526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1eb557bd003ff96f%3A0x836ddfc55f0ce060!2sSparrow%20Boutique%20Hotel!5e1!3m2!1sen!2szw!4v1757867183557!5m2!1sen!2szw',
 	},
 ];
 
@@ -222,40 +205,53 @@ const Details = () => (
 				></iframe>
 			</div>
 		</div>
-		{/* <div className={styles.scheduleContainer}>
-			<h2 className={styles.date}>18.10.25</h2>
+		<div className={`mt-5 ${styles.scheduleContainer}`}>
+			<h2 className={styles.date}>18.10.25:Schedule of Events</h2>
 			<div className={styles.timeline}>
-				<div className={styles.line} />
-				<h2 className={styles.scheduleTitle}>Schedule of Events</h2>
-				<div className={styles.schedule}>
-					<h3>We will update you with the event schedule as it becomes available.</h3>
-				</div> */}
-
-		{/* {scheduleData.map((event, index) => (
-					<div
-						key={index}
-						className={styles.event}
-					>
-						<p className={styles.time}>{event.time}</p>
-						<div className={styles.details}>
-							<h3>{event.title}</h3>
-							{event.location && <p>{event.location}</p>}
-							{event.address && <p>{event.address}</p>}
-							{event.directionsLink && (
-								<a
-									href={event.directionsLink}
-									target='_blank'
-									rel='noopener noreferrer'
-								>
-									Get Directions
-								</a>
-							)}
-							{event.description && <p className={styles.description}>{event.description}</p>}
+				{scheduleData.map((event, index) => {
+					const iconMap = {
+						Ceremony: '001-wedding-rings.png',
+						Photos: '002-photo-camera.png',
+						Reception: '003-dinner.png',
+						'Cake Cutting': '004-wedding-cake.png',
+						Dance: '005-gramophone.png',
+						'Grand Exit': '006-wedding-car.png',
+					};
+					const iconSrc = `/img/icons/${iconMap[event.title] || 'default.png'}`;
+					return (
+						<div
+							key={index}
+							className={styles.event}
+							style={{ display: 'flex', alignItems: 'center', gap: '1em' }}
+						>
+							<img
+								src={iconSrc}
+								alt={event.title + ' icon'}
+								style={{ width: '4em', height: '4em', objectFit: 'contain', minWidth: '2em' }}
+							/>
+							<div style={{ flex: 1 }}>
+								<p className={styles.time}>{event.time}</p>
+								<div className={styles.details}>
+									<h3>{event.title}</h3>
+									{/*{event.location && <p>{event.location}</p>}
+										{event.address && <p>{event.address}</p>}
+									   {event.directionsLink && (
+										   <a
+											   href={event.directionsLink}
+											   target='_blank'
+											   rel='noopener noreferrer'
+										   >
+											   Get Directions
+										   </a>
+									   )} */}
+									{event.description && <p className={styles.description}>{event.description}</p>}
+								</div>
+							</div>
 						</div>
-					</div>
-				))} */}
-		{/* </div>
-		</div> */}
+					);
+				})}
+			</div>
+		</div>
 	</>
 );
 
